ebcfda3e3ec25112828119be26e627d832037601
toazthom <109962138+toazthom@users.noreply.github.com>
2024-10-07 11:48:39 -0400

    Remodeled website from flask app to static site

--- flask_app/routes.py
+++
@@ -1,7 +0,0 @@
-# Author: Prof. MM Ghassemi <ghassem3@msu.edu>
-from flask import current_app as app
-from flask import render_template
-
-@app.route('/<page>')
-def route(page):
-	return render_template(page)
